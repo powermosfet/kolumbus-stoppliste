07d4319944e20b67ff9ab38a911ca55986f5a810
Asmund Berge <herrasbe@gmail.com>
2016-05-08 21:50:42 +0200

    add update_stops.py script

---
+++ bin/update_stops.py
@@ -0,0 +1,34 @@
+import io, requests, zipfile
+from contextlib import closing
+from gtfs.models import Stop
+
+url = "http://www.rkt.no/gt/google_transit.zip"
+
+print "Fetching new stop list from", url
+r = requests.get(url)
+if r.statuscode = 200:
+    print "File downloaded. Processing..."
+    new, changed = 0 ,0
+    with closing(r), zipfile.ZipFile(io.BytesIO(r.content)) as archive:
+        stops = archive.open('stops.txt')
+        iterstops = iter(stops)
+        next(iterstops)
+        for s in iterstops:
+            stop_id,stop_name,stop_desc,stop_lat,stop_lon,stop_url = s.split(",")
+            try: 
+                stop = Stop.objects.get(pk = stop_id)
+            except DoesNotExist:
+                stop = Stop()
+                new += 1
+            else:
+                changed += 1
+            s.stop_id   = stop_id
+            s.stop_name = stop_name
+            s.stop_desc = stop_desc
+            s.stop_lat  = stop_lat
+            s.stop_lon  = stop_lon
+            s.stop_url  = stop_url            
+            s.save()
+    print "Done processing file.", new "new,", changed, "changed."
+else:
+    print "Error fetching file:", r.statuscode
