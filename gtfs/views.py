90e65d8890cb9ef0230615bfd5f52db2235701f6
Asmund Berge <herrasbe@gmail.com>
2016-05-09 18:32:25 +0200

    use a flat json serializer

--- gtfs/views.py
+++ gtfs/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
-from django.core import serializers
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
 from gtfs.models import Stop
+from flatjsonserializer import FlatJsonSerializer
 
 def stop_list(r):
-    return HttpResponse(serializers.serialize("json", Stop.objects.all()))
+    s = FlatJsonSerializer()
+    return HttpResponse(s.serialize("json", Stop.objects.all()))
 
 def stop_detail(r, stop_id):
+    s = FlatJsonSerializer()
     try:
         stop_ob = Stop.objects.get(pk = stop_id)
     except ObjectDoesNotExist:
         return HttpResponse("The stop could not be found", status_code = 404)
-    return HttpResponse(serializers.serialize("json", stop_ob))
+    return HttpResponse(s.serialize("json", stop_ob))
