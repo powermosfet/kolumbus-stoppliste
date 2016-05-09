cfc315341204899b152f9ad21f1de27383ac9954
Asmund Berge <herrasbe@gmail.com>
2016-05-09 18:16:42 +0200

    return json instead of xml

--- gtfs/views.py
+++ gtfs/views.py
@@ -5,11 +5,11 @@ from django.core.exceptions import ObjectDoesNotExist
 from gtfs.models import Stop
 
 def stop_list(r):
-    return HttpResponse(serializers.serialize("xml", Stop.objects.all()))
+    return HttpResponse(serializers.serialize("json", Stop.objects.all()))
 
 def stop_detail(r, stop_id):
     try:
         stop_ob = Stop.objects.get(pk = stop_id)
     except ObjectDoesNotExist:
         return HttpResponse("The stop could not be found", status_code = 404)
-    return HttpResponse(serializers.serialize("xml", stop_ob))
+    return HttpResponse(serializers.serialize("json", stop_ob))
