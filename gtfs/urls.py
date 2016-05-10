8477da0e0dbe4a2a217c9c8c9f8676d5ee25cd30
Asmund Berge <herrasbe@gmail.com>
2016-05-10 10:40:40 +0200

    forgot as_view()

--- gtfs/urls.py
+++ gtfs/urls.py
@@ -3,5 +3,5 @@ from gtfs.views import stop_list, StopDetail
 
 urlpatterns = [
     url(r'^stops/$', stop_list),
-    url(r'^stops/(?P<stop_id>[0-9]+)/$', StopDetail),
+    url(r'^stops/(?P<stop_id>[0-9]+)/$', StopDetail.as_view()),
 ]
