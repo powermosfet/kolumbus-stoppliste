25e89dff181b74d129277bbca6c441dd3622e838
Asmund Berge <herrasbe@gmail.com>
2016-05-10 23:19:30 +0200

    use sqlite locally

--- stoppliste/settings.py
+++ stoppliste/settings.py
@@ -76,7 +76,8 @@ WSGI_APPLICATION = 'stoppliste.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600)
+    'default': dj_database_url.config(conn_max_age=600,
+                                      default='sqlite:///dev-database.sqlite')
 }
 
 
