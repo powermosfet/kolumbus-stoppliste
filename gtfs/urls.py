90b01c72bb1e8ac0178ae9f586ea1b49d3d239b2
Asmund Berge <herrasbe@gmail.com>
2016-05-13 22:52:43 +0200

    remove coords from url

--- gtfs/urls.py
+++ gtfs/urls.py
@@ -4,5 +4,5 @@ from gtfs.views import StopList, StopDetail, ClosestStopList
 urlpatterns = [
     url(r'^stops/$', StopList.as_view()),
     url(r'^stops/(?P<pk>[0-9]+)/$', StopDetail.as_view()),
-    url(r'^stops/closest/(?P<coords>[0-9.]+,[0-9.]+)/$', ClosestStopList.as_view()),
+    url(r'^stops/closest/$', ClosestStopList.as_view()),
 ]
