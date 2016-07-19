a79ab2f8942cf8f0609ab7563fcc483383ca67b1
Asmund Berge <herrasbe@gmail.com>
2016-07-19 10:08:47 +0200

    Fix module name in update_stops.py

--- bin/update_stops.py
+++ bin/update_stops.py
@@ -1,5 +1,5 @@
 import os, django
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stoppliste.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pebble_sanntid_backend.settings")
 django.setup()
 
 import io, requests, zipfile
